c321b8d811f9701b0c0b76902a6d534f8e4c65d6
KyuSeungSim <66705345+KyuSeungSim@users.noreply.github.com>
2022-01-31 20:49:56 -0600

    Add files via upload

---
+++ Fractal.java
@@ -0,0 +1,30 @@
+/*
+ * Project: Class Assignment Week 13
+ * Group: 11
+ * Name: Nicholas Daniels, KyuSeung Sim
+ * Class: COMP SCI 316-0800
+ * Date: DEC 01 2020
+ */
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class Fractal extends Application {
+   @Override
+   public void start(Stage stage) throws Exception {
+      Parent root = 
+         FXMLLoader.load(getClass().getResource("Fractal.fxml"));
+      
+      Scene scene = new Scene(root);
+      stage.setTitle("Fractal");
+      stage.setScene(scene);
+      stage.show();
+   }
+
+   public static void main(String[] args) {
+      launch(args);
+   }
+}
